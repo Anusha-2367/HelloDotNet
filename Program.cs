d122af145a61595371253b7785f4e7bd150c7293
YourGitHubUsername <Anusha-daredla>
2026-07-31 12:30:42 +0530

    Adding Hello .NET message

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
-﻿string name="Anusha";
-int age=23;
-string college="ABC college";
-
-Console.WriteLine(name);
-Console.WriteLine(age);
-Console.WriteLine(college);
+﻿Console.WritLine("Hello .NET");
+Console.WritLine("Learning Git Brnching");
